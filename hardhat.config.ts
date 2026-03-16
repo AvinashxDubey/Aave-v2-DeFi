ad05303ecc8d81c8db3bee3a2ce30c68351782aa
Avinash Dubey <speedofreigns619@gmail.com>
2026-03-16 16:01:17 +0530

    changed config for sepolia deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      // Use a single deployer account from an environment variable
+      accounts: process.env.SEPOLIA_PRIVATE_KEY
+        ? [process.env.SEPOLIA_PRIVATE_KEY]
+        : [],
+    },
+  },
+  etherscan: {
+    // Etherscan v2 config: single apiKey string
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
+  },
 };
 
 export default config;
